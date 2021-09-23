ee8436e6105a4dcc568e5459b52f76002cf8c9d5
Frank Pacini <fgpacini@yahoo.com>
2021-09-23 11:53:11 -0700

    school

--- 01-python/education/school.py
+++ 01-python/education/school.py
@@ -1,3 +1,62 @@
 def draw_school():
-    print("school not found")
+    print("""
+                                                                                                                                                                                  
+                                                                                                                                                                              
+                                                                                                                                                                              
+                                                                          ``...---::::::::::---...``                                                                          
+                                                                   ``.--:::://////////////////////::::--.``                                                                   
+                                                               `.-:::////////////////////////////////////:::-.`                                                               
+                                                           `.-:://///////////////////+ooo+++o/////////////////::-.`                                                           
+                                                        `.-://///////////////////////o++o///yo++oo////////////////:-.`                                                        
+                                                     `.-:////////////////////////////o++o/::y/::+y+//////////////////:-.`              .:.:                                   
+                                                   `-:///////////////////////////////o++o+//y+/:+y+/////////////////////:-`            .yys                                   
+                                                 `-//////////////////////////////////o++sossy+++os+////////////////////////-`          .:-:                                   
+                                               `-////////////////////////////////////o+oo+/+++ooo+///////////////////////////-`                                               
+                                             `-//////////////////////////////////////s+oo+/////////////////////////////////////-`              `                              
+                        `//`                -///////////////////////////////++oooo++//////+++oooo+///////////////////////////////-            `s+                             
+                        :yy/              `://///////////////////////////+so/-`                `.:+so/////////////////////////////:`                                          
+                         ``              -/////////////////////////////os:`        /`           `/ .:os+////////////////////////////-                                         
+                                        -//////////////////////////os++/`   o+`-+- o:+-.+-o`+:/:.s..../ooos//////////////////////////-                                        
+                  ``                  `:///////////////////////////y.       :/-./- /`:-./-+`/:/:.o-.......yo//////////////////////////:`                                      
+                  oo`                `:///////////////////////////+y-........-..--.------:---::--:--------ys++/////////////////////////:`                                     
+                  ``                `:////////////////////////+sooosoooooooooooooooooooooooooooooooooooooosoooy+////////////////////////:`                                    
+                                    ://///////////////////////os/oooooooooooooooooooooooooooooooooooooooooooooyo+////////////////////////:                                    
+                                   -//////////////////////////oy+oooooooooooooooooooooooooooooooooooooooooooooyo+/////////////////////////-                                   
+                         -+++++++++oooooooooooooooooooooooooooosyy+oooooooooooooooooooooooooooooooooooooooooyysssoooooooooooooooooooooooooo+++++++++-                         
+                        -y/+++++++++++++++++++++++++++++++++++++ss.--:://////::---:://////::---:://////::---ss+++++++++++++++++++++++++++++++++++++oy-                        
+                        -y+oooooooooooooooooooooooooooooooooooooss`--+soo++++so---osoo++++so---osoo++++s+---ssooooooooooooooooooooooooooooooooooooooy-                        
+                         ./++yooooooooooooooooooooooooooooooooosss`--oo:----:os---so:----:os---so:----:oo---sssoooooooooooooooooooooooooooooooosy++/.                         
+                             y:-:::::::::::::::::::::::::::::::/os`--o+...--:+s---s+...--:os---s+...--:oo---so/::::::::::::::::::::::::::::::::+y                             
+                             y:.--/+++++++++:-----:+++++++++/--:+s`--o+---:::+s---s+---:::+s---s+---:::oo---s+:--/+++++++++:-----:+++++++++/--:+y                             
+                             y:.--y`------.:s-----s-.------`y--:+s`--oo--::--+s---s+--::--+s---s+--::--+o---s+:--y`------.-s-----s:.------`y--:+y                             
+                             y:.--y s////o/-s-----s-++////s y--:+s`--oo:::-.-+s---so:::-.-+s---s+:::-.-oo---s+:--y s////++-s-----s-/o////s y--:+y                             
+                             y:.--y s::::+/-s-----s-+/::::s y--:+s`--oo:----:+s---so:----:os---s+:----:oo---s+:--y s::::/+-s-----s-/+::::s y--:+y                             
+                             y:.--y s:::://-s-----s-+/::::s y--:+s`-/so++++ooos+-/so++++ooos/-+so++++ooos/--s+:--y s::::/+-s-----s-//::::s y--:+y                             
+                             y:.--y s:::://-s-----s-+/::::s y--:+s`-so::::::::os-s+::::::::os-s+::::::::os--s+:--y s::::/+-s-----s-//::::s y--:+y                             
+                             y:.--y s:::://-s-----s-+/::::s y--:+s`-:++++++++++:-:++++++++++:-:++++++++++:--s+:--y s::::/+-s-----s-//::::s y--:+y                             
+                             y:.-:y.s////++:s-----s:++////s.y:-:+s`-----------------------------------------s+:-:y.s////++:s-----s:++////s.y:-:+y                             
+                             y:.o++/++++++++os/-/o++++++++o+ooo:+s`-+oooooo+++oooooooooooooooooooooooooooo:-s+:o++/++++++++os/-/o++++++++o+ooo:+y                             
+                             y:.+oooooooooooos/-/ooooooooooooo+:+s`+o+++++++++++++++++++++++++++++oooooooss-s+:+oooooooooooos/-/ooooooooooooo+:+y                             
+                             y:.-:::::::::::::---:::::::::::::-:+s`o+oooooooooooooooooooooooooooooooooooooy-s+:-:::::::::::::---:::::::::::::-:+y                             
+                             y:.--++++++++++/-----/++++++++++--:+s`o+oooooooooooooooooooooooooooooooooooooy-s+:--++++++++++/-----/++++++++++--:+y                             
+                  `          y:.--y :::::/--s-----s--::::::`y--:+s`o+oooooooooooooooooooooooooooooooooooosy-s+:--y`::::::--s-----s--/::::: y--:+y                             
+                 .o-`        y:.--y s////+/-s-----s-++////s y--:+s`:oy+ssoooooooooooooooooooooooooooosy+ss/-s+:--y s////++-s-----s-/+////s y--:+y                             
+                `:y/.        y:.--y s::::+/-s-----s-+/::::s y--:+s`-:y.+s::::::::::::::::::::::::::::+s.s+--s+:--y s::::/+-s-----s-/+::::s y--:+y                             
+                  `          y:.--y s:::://-s-----s-+/::::s y--:+s`-:y`+s-oooooooooooo--oooooooooooo-+s o+--s+:--y s::::/+-s-----s-//::::s y--:+y                             
+                          .::y+:::y s:::://-s---:/y+o+::::s y--:+s`-:y`/s:y::::o+:::/y--y/:::+o::::y:+s o+--s+:--y s::::+o+y/:---s-//::::s y::+oy::.                          
+            ./.        `/o+////+osy s:::://-s:oo/////+os/:s y--:+s`-:y`/s:y::::o/:::/y--y/:::+o::::y:+s o+--s+:--y s:/so/////+oo:s-//::::s ys+/////+o/`                       
+            ./.       .s/:/++++++/osy++++oo/ys::/+++++++soy-y/-:+s`-:y`/s:y::::o/:::/y--y/:::+o::::y:+s o+--s+:-/y-yoo://+++++/+yy/oo++++ys+:/++++++/os.                      
+                      s//++++o+++++ss/////+oy:/++++o+++++yo++os:+s`-:y`/s:y::::o/:::/y--y/:::+o::::y:+s o+--s+:s+//os:/+++++++++oy+//////s+:/+++o+++++os                      
+                      y:+++ooy+++++oyoooooosy:+++oss+++++ssooo/:+s`-:y`/s:y::::o/:::/y--y/:::+o::::y:+s o+--s+:/oooso/+++sso+++++yoooooooy//+++oy+++++oy     :+.              
+                      +o/++++yoo+++y+:::::::s+++++oys+++oy/::--:+s`-:y`/s:y::::o/:::/y--y/:::+o::::y:+s o+--s+:--::/y/++++sss+++os:::::::+s/++++yso+++s+     :+.              
+                      `sso+++yo++ossoooooooosss+++oyo+oossooooooss`++o/+++o++++++++++o//o++++++++++o+oo+ooo-ssoooooosso+++ss++osssoooooooosso+++yo++oss`                      
+                      /s+ysssysssso+++++++++++ossssyssso+++ooooosy:y-----------------------:::::::::::::::s+yso+++++++osssssssso+++++++++++ossssysssy+s/                      
+                  `-/+s:so/ooyo++++++++++++++++++ooyoo++ooooooooy-------------------------------:::::::::::::ss++++++++++osso+++++++++++ooooooooysooss/s+/-`                  
+                 :o+////y++++y++++++++++++++++++++oyoooooooooosos///////////////////////////+++++++++++++++++osso+++++++++ss++++++ooooooooooooooyooosy////+o:                 
+              ../y/+++++yooooyoooooooooooooooooooosyoooooooooso::::::::::::::::::::::::::::::::////////////////+yooooooooossooooooooooooooooooooysoosy+++++/y/..              
+         `````+oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo+`````         
+        ````.............----------------------------------------------------------------------------------------------------------------------------.............````        
+                                      `````````..........................................................```````````````````````````                                           
+    
+    """)
     return
